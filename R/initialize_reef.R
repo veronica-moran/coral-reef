c0065e7f4fda258dca3b5f5c8967cc96749cf181
veronica-moran <moranv620@gmail.com>
2026-08-12 18:42:48 -0700

    initialize corrected

--- R/initialize_reef.R
+++ R/initialize_reef.R
@@ -1,16 +1,33 @@
 set.seed(32)  #Define Randomness
 
-reef_matrix <-matrix(data=0
+reef<-matrix(data=0
     , nrow=5, ncol=5)
-reef_matrix
-coral_matrix
+reef
+reef
 seq(1,25)
 sample(1:25, size=8, replace=FALSE)
 random <- sample(1:25, size=8, replace=FALSE)
 
 
 
-# Rows 
-(1:25 -1) %% 5 +1
-cell_label<-sample(1:25, size=8)
-cell_row<-(cell_label-1)%/%5 -1
+# Rows
+# Working Through the Logic
+1:25%%5
+(1:25-1)%%5
+(1-25-1)%%5+1
+(1:25-1)%/%5+1
+#Done working through
+
+#Rows
+(1:25-1)%/%5+1 #Correct code for Rows
+
+#Columns
+(1:25-1)%%5+1
+
+
+cell_label<-6
+cell_row<-(6-1)%/%5+1
+cell_col<-(6-1)%%5+1
+reef[cell_row,cell_col]<-1
+
+reef
